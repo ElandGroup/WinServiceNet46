7d9dcff411af64b0ef344564579e40fc56181477
elandcloud <xiaoxm_001@outlook.com>
2016-11-09 09:26:44 +0800

    winservice .net 46

---
+++ WinServiceNet46.Data/Jobs/SimpleJob.cs
@@ -0,0 +1,19 @@
+﻿using Common.Logging;
+using Quartz;
+using WinServiceNet46.Data.Service;
+
+namespace WinServiceNet46.Data.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class SimpleJob : IJob
+    {
+        ILog log = LogManager.GetLogger("Logger");
+        public void Execute(IJobExecutionContext context)
+        {
+            log.Info("SimpleJob");
+        }
+
+    }
+
+
+}
---
+++ WinServiceNet46/WinServiceNet46.cs
@@ -0,0 +1,27 @@
+﻿using Common.Logging;
+using WinServiceNet46.Data;
+using System.ServiceProcess;
+
+namespace WinServiceNet46
+{
+    public partial class WinServiceNet46 : ServiceBase
+    {
+        ILog log = LogManager.GetLogger("Logger");
+
+        public WinServiceNet46()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            JobHelper.JobLoader();
+            log.Info("WinServiceNet46 Started");
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("WinServiceNet46 Stoped");
+        }
+    }
+}
